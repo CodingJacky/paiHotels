25d61410ad4ef7895e348a4a758bf13a76321d38
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2023-09-28 21:30:16 +0530

    Update Find.js
    when finding hotels, In query the case of city name is made capital for uniformity

--- frontend/src/components/Find/Find.js
+++ frontend/src/components/Find/Find.js
@@ -24,9 +24,9 @@ const Find = (props) => {
      const sendIt = (e) => {
           e.preventDefault();
           if(passJson.find.name === "") passJson.find.name = "null";
-          else passJson.find.name = passJson.find.name.toUpperCase();
           if(passJson.find.city === "") passJson.find.city = "null";
-          else passJson.find.city = passJson.find.city.toUpperCase();
+          if (passJson.find.name !== "null") passJson.find.name = passJson.find.name.toUpperCase();
+          if (passJson.find.city !== "null") passJson.find.city = passJson.find.city.toUpperCase();
           props.ultimateSearch(passJson);
      }
      
